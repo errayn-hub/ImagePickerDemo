007b62b164fe6bb6c5141f1126e8be9f820de76a
errayn-hub <128374386+errayn-hub@users.noreply.github.com>
2023-04-10 23:44:23 +0300

    Initial Commit

---
+++ image Picker Demo/image_Picker_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_Picker_DemoApp.swift
+//  image Picker Demo
+//
+//  Created by ErRayn on 10.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct image_Picker_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ image Picker DemoUITests/image_Picker_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  image_Picker_DemoUITestsLaunchTests.swift
+//  image Picker DemoUITests
+//
+//  Created by ErRayn on 10.04.2023.
+//
+
+import XCTest
+
+final class image_Picker_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
